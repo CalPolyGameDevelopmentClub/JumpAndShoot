13a1af94794a7f2de4ca54e3a814fd084150e23f
Kyle Piddington <kylepiddington@pcp078530pcs.wireless.calpoly.edu>
2016-11-03 09:12:35 -0700

    Sound Prototype in place

---
+++ Assets/Standard Assets/2D/Scripts/Platform2DProjectile.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platform2DProjectile : MonoBehaviour {
+	public float m_velocity;
+	public float lifeTime;
+	private Vector3 m_direction;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifeTime--;
+		if(lifeTime < 0)
+		{
+			Destroy(gameObject);
+		}
+		transform.Translate(m_velocity * m_direction * Time.deltaTime,Space.World);
+
+	}
+
+	public void SetDirection(Vector3 dir)
+	{
+		m_direction = dir;
+	}
+
+	void OnCollisionEnter()
+	{
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Standard Assets/2D/Scripts/Platformer2DFire.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platformer2DFire : MonoBehaviour {
+	public Transform fireSpawnPosition;
+	public Platform2DProjectile m_BulletPrefab;
+	public Platform2dSFX m_sfx;
+	// Use this for initialization
+	public void Fire(bool fire, bool flipped)
+	{
+		if(fire && m_BulletPrefab != null)
+		{
+			Quaternion rot = m_BulletPrefab.transform.rotation;
+			if(flipped)
+			{
+	            rot = Quaternion.Euler(0,0,90);
+			}
+			Platform2DProjectile proj = Instantiate(m_BulletPrefab, fireSpawnPosition.position, rot) as Platform2DProjectile;
+			
+			proj.SetDirection(transform.right * (flipped? -1.0f : 1.0f));
+			if(m_sfx != null)
+			{
+				m_sfx.PlayShootSFX();
+			}
+		}
+	}
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
